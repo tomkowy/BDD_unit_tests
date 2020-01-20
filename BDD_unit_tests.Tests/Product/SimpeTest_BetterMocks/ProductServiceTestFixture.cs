2e67b67da54e57e652ce715ffc01d5270c371bf8
Tomek Marchewka <tomasz.marchewka@codibly.com>
2020-01-20 09:31:42 +0100

    simple test better mocks

---
+++ BDD_unit_tests.Tests/Product/SimpeTest_BetterMocks/ProductServiceTestFixture.cs
@@ -0,0 +1,47 @@
+﻿using BDD_unit_tests.Common.Exceptions;
+using BDD_unit_tests.Product.Services;
+using BDD_unit_tests.Tests.Helpers;
+using System;
+using Xunit;
+
+namespace BDD_unit_tests.Tests.Product.SimpeTest_BetterMocks
+{
+    public partial class ProductServiceTest : TestBase
+    {
+        private int _currentUserId;
+        private string _name;
+        private int _cost;
+        private string _category;
+
+        private Action _action;
+
+        private IProductService _productService;
+
+        public ProductServiceTest()
+        {
+            var dbContext = GetDbContext();
+
+            var productRepositoryMock = new ProductRepositoryMock();
+
+            var userRepositoryMock = new UserRepositoryMock();
+            userRepositoryMock.IsModeratorMock(_currentUserId, true);
+
+            _productService = new ProductService(productRepositoryMock.Object, userRepositoryMock.Object, dbContext);
+        }
+
+        private void Give_moderator()
+        {
+            _currentUserId = 2;
+        }
+
+        private void When_add_product()
+        {
+            _action = () => _productService.Add(_currentUserId, _name, _cost, _category);
+        }
+
+        private void Then_throw_user_is_not_admin_exception()
+        {
+            Assert.Throws<UserIsNotAdmin>(_action);
+        }
+    }
+}
