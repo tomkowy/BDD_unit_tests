c52ab376af9d61982e91d4a11dc466d3575699e4
Tomek Marchewka <tomasz.marchewka@codibly.com>
2020-01-20 09:38:28 +0100

    complex test better setup

---
+++ BDD_unit_tests.Tests/Product/ComplexTest_BetterSetup/UserRepositoryMock.cs
@@ -0,0 +1,22 @@
+﻿using BDD_unit_tests.User.Repository;
+using Moq;
+
+namespace BDD_unit_tests.Tests.Product.ComplexTest_BetterSetup
+{
+    public class UserRepositoryMock : Mock<IUserRepository>
+    {
+        public UserRepositoryMock IsAdminMock(int userId, bool output)
+        {
+            Setup(x => x.IsAdmin(userId))
+            .Returns(output);
+            return this;
+        }
+
+        public UserRepositoryMock IsModeratorMock(int userId, bool output)
+        {
+            Setup(x => x.IsModerator(userId))
+            .Returns(output);
+            return this;
+        }
+    }
+}
