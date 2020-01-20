5c63cc1cd07d600083f86ff2290333e60f528950
Tomek Marchewka <tomasz.marchewka@codibly.com>
2020-01-20 09:09:32 +0100

    add simple bdd test

---
+++ BDD_unit_tests.Tests/Product/SimpleTest/ProductServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit2;
+
+namespace BDD_unit_tests.Tests.Product.SimpleTest
+{
+    public partial class ProductServiceTest
+    {
+        [Scenario]
+        public void Cannot_add_product_when_user_is_not_admin()
+        {
+            Runner.RunScenario(
+                Give_moderator,
+                When_add_product,
+                Then_throw_user_is_not_admin_exception
+                );
+        }
+    }
+}
---
+++ BDD_unit_tests.Tests/Product/SimpleTest/ProductServiceTestFixture.cs
@@ -0,0 +1,53 @@
+﻿using BDD_unit_tests.Common.Exceptions;
+using BDD_unit_tests.Product.Repository;
+using BDD_unit_tests.Product.Services;
+using BDD_unit_tests.Tests.Helpers;
+using BDD_unit_tests.User.Repository;
+using LightBDD.XUnit2;
+using Moq;
+using System;
+using Xunit;
+
+[assembly: LightBddScopeAttribute]
+
+namespace BDD_unit_tests.Tests.Product.SimpleTest
+{
+    public partial class ProductServiceTest : TestBase
+    {
+        private int _currentUserId;
+        private string _name;
+        private int _cost;
+        private string _category;
+
+        private Action _action;
+
+        private IProductService _productService;
+
+        public ProductServiceTest()
+        {
+            var dbContext = GetDbContext();
+
+            var productRepository = new Mock<IProductRepository>();
+
+            var userRepository = new Mock<IUserRepository>();
+            userRepository.Setup(x => x.IsModerator(2)).Returns(true);
+
+            _productService = new ProductService(productRepository.Object, userRepository.Object, dbContext);
+        }
+
+        private void Give_moderator()
+        {
+            _currentUserId = 2;
+        }
+
+        private void When_add_product()
+        {
+            _action = () => _productService.Add(_currentUserId, _name, _cost, _category);
+        }
+
+        private void Then_throw_user_is_not_admin_exception()
+        {
+            Assert.Throws<UserIsNotAdmin>(_action);
+        }
+    }
+}
