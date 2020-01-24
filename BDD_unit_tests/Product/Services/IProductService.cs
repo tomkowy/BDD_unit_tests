56b716fde29b26f1181d3813d1a317a04bfba366
Tomek Marchewka <tomasz.marchewka@codibly.com>
2020-01-24 07:35:08 +0100

    add formatting for prezentation

--- BDD_unit_tests/Product/Services/IProductService.cs
+++ BDD_unit_tests/Product/Services/IProductService.cs
@@ -2,8 +2,10 @@
 {
     public interface IProductService
     {
-        void Add(int currentUserId, string name, int cost, string category);
-        void Update(int currentUserId, int id, string name, int cost, string category);
+        void Add(int currentUserId, string name, 
+            int cost, string category);
+        void Update(int currentUserId, int id, 
+            string name, int cost, string category);
         void Remove(int currentUserId, int id);
     }
 }
